cdee739a41311f1a2e1d18e192e137943da16ace
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-10-26 20:17:06 +0700

    chore: Dts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 import { checker } from 'vite-plugin-checker'
+import dts from 'vite-plugin-dts'
 import svgr from 'vite-plugin-svgr'
 
 import linaria from '@linaria/vite'
@@ -57,6 +58,7 @@ export default defineConfig(({ mode }) => {
 					],
 				},
 			}),
+			dts(),
 		],
 		resolve: {
 			alias: resolveAlias,
